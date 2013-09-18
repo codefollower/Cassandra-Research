558969e4e8b58fe11ea2df4cc6048e1cce0111f7
Honghua Zhu <zhh200910@gmail.com>
2013-09-19 00:15:57 +0800

    删除1.2.5的代码

--- my-docs/需要理清的基本概念.java
+++
@@ -1,9 +0,0 @@
-org.apache.cassandra.db.Table 对应的是Keyspace
-org.apache.cassandra.db.ColumnFamilyStore 才对应列族
-Table包含ColumnFamilyStore
-
-
-但是通过CQL的CREATE TABLE命令建表时，这时的表反而对应org.apache.cassandra.db.ColumnFamilyStore
-而CREATE KEYSPACE命令才是对应org.apache.cassandra.db.Table
-
-这些是历史问题
